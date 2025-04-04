d0fe8f1b5b1eca7d673033ad009ffc0311eb6d5d
MotorolaMan173 <143439269+MotorolaMan173@users.noreply.github.com>
2025-04-04 16:18:31 -0400

    added takepicture and request permission methods

--- PhotoApp/app/src/main/java/edu/gannon/photoapp/MainActivity.java
+++ PhotoApp/app/src/main/java/edu/gannon/photoapp/MainActivity.java
@@ -1,21 +1,30 @@
 package edu.gannon.photoapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
     Button takePhoto;
     Button savePhoto;
     ImageView imageTaken;
+
+    private static final int REQUEST_PERMISSION_STORAGE = 1;
+    private static final int REQUEST_PERMISSION_CAMERA = 2;
 
 
     @Override
@@ -37,8 +46,34 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void takePicture(View view) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestCameraPermission();
+        } else {
+            saveToStorage();
+        }
     }
+
+    private void saveToStorage() {
+        
+    }
+
+    private void requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Toast.makeText(this, "Permissions Requiered to take photos", Toast.LENGTH_LONG).show();
+
+        }
+        ActivityCompat.requestPermissions(
+               this,
+               new String[]{Manifest.permission.CAMERA},
+               REQUEST_PERMISSION_CAMERA
+
+        );
+    }
+
 
     public void savePicture(View view) {
     }
+
+
 }
